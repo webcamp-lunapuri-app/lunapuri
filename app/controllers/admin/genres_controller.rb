45cbf356b70a83c5bc855ac3a68e9eec01b4a0b0
sakio318 <saki1803terashima@gmail.com>
2021-01-06 10:17:45 +0000

    make_products_genres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
+    @new_genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @new_genre = Genre.new(genre_params)
+    @new_genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
   end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+
 end
