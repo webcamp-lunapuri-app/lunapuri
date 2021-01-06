5f6dd0622ca7147d3edd9324be73cbd06ee1e6c4
HidekiShiba <hshiba033@gmail.com>
2021-01-06 10:16:56 +0000

    [Add] 会員用商品一覧

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,10 @@
 class Public::ProductsController < ApplicationController
+  PER = 8
   def index
+    @products = Product.page(params[:page]).per(PER).order(created_at: :desc)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
