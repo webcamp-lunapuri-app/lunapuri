174b28d96ab417d350495b2293e44f746e5c0a66
kazutokato <e.t.s.w.a.n.a@gmail.com>
2021-01-08 02:29:48 +0000

    making deliveries

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,7 +1,31 @@
 class Public::DeliveriesController < ApplicationController
   def index
+    @delivery = Delivery.new
+    @client = current_client
+    @deliveries = @client.deliveries.page(params[:page]).reverse_order
+  end
+
+  def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.save
+    redirect_back(fallback_location: root_path)
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @delivery = Delivery.find(params[:id])
+    @delivery.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def delivery_params
+    params.require(:delivery).permit(:client_id, :postal_code, :address, :name)
   end
 end
