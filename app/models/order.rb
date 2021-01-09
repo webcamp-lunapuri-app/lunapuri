f975b6cc87e6ef615dd4e2adad6a9c027700afd3
sakio318 <saki1803terashima@gmail.com>
2021-01-09 10:43:59 +0000

    making_order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :client
-  has_many :order_items, 
-  
+  has_many :order_items,
+
   enum payment_methods: {クレジットカード: 0, 銀行振込: 1}
   enum order_status: {入金待ち: 0, 入金確認済: 1, 製作中: 2, 発送準備中: 3, 発送済: 4}
 end
